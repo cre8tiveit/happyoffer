4781430046c7bd6f4d038e18be840710fae005b9
henkjurriens70 <henk.jurriens@gmail.com>
2024-03-12 17:04:13 +0100

    added login

--- src/app/modules/authentication/pages/login/login.page.ts
+++ src/app/modules/authentication/pages/login/login.page.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.page.html',
   styleUrls: ['login.page.scss'],
 })
-export class LoginPage {
+export class LoginPage implements OnInit {
   public loginForm: any;
 
-  constructor() {}
+  constructor(private readonly router: Router) {}
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required),
+    });
+  }
 
   public login(): void {
-    console.log('login');
+    this.router.navigate(['/home']);
   }
 }
